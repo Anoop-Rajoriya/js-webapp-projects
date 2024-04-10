15417632ec5c545dd72d6ea3d58f3b9b81fde4a6
Anoop Rajoriya <anooprajoriya9827@gmail.com>
2024-04-10 22:19:54 +0530

    today work

---
+++ Focus on Today webapp/scripts/script.js
@@ -0,0 +1,7 @@
+console.log("script is running!");
+
+const goalsContainer = document.querySelector(".goals-container");
+const checkBox = goalsContainer.querySelectorAll(".check-box");
+const goalsInp = goalsContainer.querySelectorAll(".check-box + input");
+const progressBar = document.querySelector(".progress-bar")
+const errorElm = document.querySelector(".error")
