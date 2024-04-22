63071c7edca74fcdb9ae4234772c80ee7025e48a
Anoop Rajoriya <anooprajoriya9827@gmail.com>
2024-04-22 16:29:29 +0100

    game done for working not completed

--- Quiz Time webapp/scripts/app.js
+++ Quiz Time webapp/scripts/app.js
@@ -1,104 +1,135 @@
-const quizeData = {
+let quizeData = {
     queArr: [],
     isPlayer: false,
     score: "0",
+    gameCount: 0,
 };
 
-let count = 1;
+const questionElm = document.querySelector(".question-container");
+const optionElmArr = document.querySelectorAll(".option");
+const appCounter = document.querySelector(".question-counter");
+const appTimer = document.querySelector(".question-timer");
+const nextElm = document.querySelector(".next");
 
-const questionElm = document.querySelector('.question-container');
-const optionElmArr = document.querySelectorAll('.option')
+let showAnsTimeout;
+let canNext = false;
 
-const gameStart = () => {
-    console.log('Game start!!');
-    // console.log(quizeData);
-    console.log(quizeData.queArr[count]);
-
-    // console.log(atob(quizeData.queArr[count].que));
-    questionElm.innerText = atob(quizeData.queArr[count].que)
-    // let randomKey = Math.floor(Math.random() * 4);
-    // console.log(randomKey);
-    // console.log(optionElmArr[randomKey]);
-    let keyElm = optionElmArr[Math.floor(Math.random() * 4)];
-    keyElm.innerText = atob(quizeData.queArr[count].ans);
-    // console.log(keyElm);
-
-    // console.log([...optionElmArr].filter(e=> e !== keyElm));
-    const filteredNodeList = [...optionElmArr].filter(e=> e !== keyElm);
-    // console.log(filteredNodeList, quizeData.queArr[count].option);
-    console.log(Object.values(quizeData.queArr[count].option));
-    Object.values(quizeData.queArr[count].option).forEach((opt, index) => {
-        filteredNodeList[index].innerText = atob(opt);
-    })
-    
-}
-
-
-function fetchData(data) {
-    // console.log(data?.results);
-    if (data.results) {
-        data.results.forEach((element) => {
-            let question = {
-                que: element.question,
-                ans: element.correct_answer,
-                option: {
-                    op1: "",
-                    op2: "",
-                    op3: "",
-                },
-            };
-            Object.keys(question.option).forEach((key, index) => {
-                question.option[key] = element.incorrect_answers[index];
-                // console.log('value pused!');
-            });
-            // console.log(question);
-            quizeData.queArr.push(question);
-        });
-
-        // console.log(quizeData);
-        setTimeout(()=>  localStorage.setItem("quizeData", JSON.stringify(quizeData)), 1000)
-        console.log('data fatched!!!');
-        gameStart();
-    }
-}
-
-function fetchStoreData() {
-    const data = JSON.parse(localStorage.getItem('quizeData'));
-    // console.log(data.queArr);
-
-    data.queArr.forEach((elm) => {
-        // console.log(elm);
-        const question = {
-            que: elm.que,
-            ans: elm.ans,
-            option: {
-                op1: elm.option.op1,
-                op2: elm.option.op2,
-                op3: elm.option.op3,
-            }
-        }
-
-        quizeData.queArr.push(question)
-        // console.log(question);
-
-    })
-
-    gameStart();
-}
-
-window.onload = () => {
-    if (!localStorage.quizeData) {
+if (!localStorage.quizeData) {
+    try {
         fetch(
             `https://opentdb.com/api.php?amount=25&category=18&difficulty=easy&encode=base64`
         )
             .then((res) => res.json())
             .then(fetchData);
+    } catch (error) {
+        console.log(error, "please reload page data not fetched!");
     }
-    else{
-        console.log("data available!!!");
-        // console.log(JSON.parse(localStorage.getItem('quizeData')));
-        fetchStoreData()
+} else fetchData();
+
+function fetchData(params) {
+    if (!localStorage.getItem("quizeData")) {
+        params.results.forEach((element) => {
+            const question = {
+                que: element.question,
+                ans: element.correct_answer,
+                option: [],
+            };
+
+            element.incorrect_answers.forEach((option) => {
+                question.option.push(option);
+            });
+
+            quizeData.queArr.push(question);
+        });
+
+        setTimeout(
+            () => localStorage.setItem("quizeData", JSON.stringify(quizeData)),
+            500
+        );
+    } else quizeData = JSON.parse(localStorage.getItem("quizeData"));
+
+    gameStart();
+}
+
+function gameStart(params) {
+    updateQuesAndAns();
+    optionElmArr.forEach((option) => {
+        option.addEventListener("click", checkUserAns, false);
+    });
+    nextElm.addEventListener("click", nextLevel, false);
+}
+
+function checkUserAns(params) {
+    if (
+        params.target.innerText ==
+        atob(quizeData.queArr[quizeData.gameCount].ans)
+    ) {
+        // console.log('currect ans');
+        params.target.classList.add("currect");
+    } else {
+        // console.log('wrong ans')
+        params.target.classList.add("wrong");
     }
-};
 
+    optionElmArr.forEach((option) => {
+        option.removeEventListener("click", checkUserAns, false);
+    });
 
+    showAnsTimeout = setTimeout((e) => {
+        optionElmArr.forEach((e) => {
+            e.classList.remove("currect");
+            e.classList.remove("wrong");
+        });
+
+        optionElmArr.forEach((e) => {
+            if (e.innerText !== atob(quizeData.queArr[quizeData.gameCount].ans))
+                e.classList.add("wrong");
+            else e.classList.add("currect");
+        });
+    }, 800);
+
+    canNext = true;
+}
+
+function nextLevel(params) {
+    console.log('hello');
+    nextElm.removeEventListener("click", nextLevel, false);
+    clearTimeout(showAnsTimeout);
+    if (!canNext) return;
+    canNext = false;
+    quizeData.gameCount++;
+    if (quizeData.gameCount <= 24) {
+        gameStart();
+    } else {
+        gameEnd();
+    }
+}
+
+function updateQuesAndAns(params) {
+    optionElmArr.forEach((e) => {
+        e.classList.remove("currect");
+        e.classList.remove("wrong");
+    });
+
+    if (quizeData.gameCount <= 24) {
+        appCounter.firstElementChild.innerText = quizeData.gameCount + 1;
+        questionElm.innerText = atob(quizeData.queArr[quizeData.gameCount].que);
+        let keyElm = optionElmArr[Math.floor(Math.random() * 4)];
+        keyElm.innerText = atob(quizeData.queArr[quizeData.gameCount].ans);
+        const filteredNodeList = [...optionElmArr].filter((e) => e !== keyElm);
+
+        filteredNodeList.forEach((option, index) => {
+            if (quizeData.queArr[quizeData.gameCount].option[index])
+                option.innerText = atob(
+                    quizeData.queArr[quizeData.gameCount].option[index]
+                );
+            else option.innerText = "option not available!";
+        });
+    } else {
+        gameEnd()
+    }
+}
+
+function gameEnd(params) {
+    console.log("game end !!!!!");
+}
