c2bad3a4cb7d58e5bc5082083b310ab36d02c978
Anoop Rajoriya <anooprajoriya9827@gmail.com>
2024-04-30 21:45:53 +0530

    all done rest contry web app completed

--- Rest Counties webapp/scripts/index.js
+++ Rest Counties webapp/scripts/index.js
@@ -1,36 +1,21 @@
-function toggleDropdown() {
-  var dropdownContent = document.getElementById("dropdown-content");
-  dropdownContent.style.display === "block"
-    ? (dropdownContent.style.display = "none")
-    : (dropdownContent.style.display = "block");
+
+let countriesData = [];
+
+fetch("https://restcountries.com/v3.1/all")
+  .then((res) => res.json())
+  .then(init);
+
+function init(data) {
+  countriesData = data;
+  showData(data);
 }
 
-function selectOption(option) {
-  var selectedOptionText = document.getElementById("selected-option");
-  selectedOptionText.textContent = option;
-  toggleDropdown(); // Close the dropdown after selecting an option
-}
+function showData(countriesArr) {
+  console.log(countriesArr);
+  document.querySelector(".card-container").innerHTML = "";
 
-
-let countriesData;
-
-  fetch("https://restcountries.com/v3.1/all")
-    .then((res) => res.json())
-    .then(fecthData);
-
-function fecthData(params) {
-    countriesData = [...params];
-    // console.log(countriesData[0].flags.svg);
-    // console.log(countriesData[0].name.common);
-    // console.log(countriesData[0]);
-    countriesData.forEach(showData);
-  }
-
-function showData(countrie, index) {
-//   console.log(index, countrie.flags.svg);
-//   console.log(index, countrie.name.common);
-
-  const cardBody = `<div class="card-top">
+  countriesArr.forEach((countrie) => {
+    const cardBody = `<div class="card-top">
                         <img
                             src=${countrie.flags.svg}
                             alt=${countrie.name.common} flag
@@ -38,19 +23,52 @@ function showData(countrie, index) {
                     </div>
                     <div class="card-bottom">
                         <h2>${countrie.name.common}</h2>
-                        <p><span>Population:</span> ${Number(countrie.population).toLocaleString("en-IN")}</p>
+                        <p><span>Population:</span> ${Number(
+                          countrie.population
+                        ).toLocaleString("en-IN")}</p>
                         <p><span>Region:</span> ${countrie.region}</p>
                         <p><span>Capital:</span> ${countrie.capital}</p>
-                    </div>`
-                
-    // console.log(card);
-    const card = document.createElement('div')
-    card.classList.add('card')
-    card.innerHTML = cardBody
-    document.querySelector('.card-container').appendChild(card)
+                    </div>`;
 
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = cardBody;
+    document.querySelector(".card-container").appendChild(card);
 
     card.addEventListener("click", (e) => {
-        location.href = `./countrie.html?countrieName=${countrie.name.common}`;
-      });
+      location.href = `./countrie.html?countrieName=${countrie.name.common}`;
+    });
+  });
+}
+
+// searching feature
+
+document
+  .querySelector(".search-container")
+  .lastElementChild.addEventListener("input", function (params) {
+    showData(
+      countriesData.filter(function (countrieObj) {
+        return String(countrieObj.name.common)
+          .toLowerCase()
+          .includes(String(params.target.value).toLowerCase());
+      })
+    );
+  });
+
+// filtering feature 
+
+function toggleDropdown() {
+  let dropdownContent = document.getElementById("dropdown-content");
+  dropdownContent.style.display === "block"
+    ? (dropdownContent.style.display = "none")
+    : (dropdownContent.style.display = "block");
+}
+
+function selectOption(option) {
+  let selectedOptionText = document.getElementById("selected-option");
+  selectedOptionText.textContent = option;
+  showData(countriesData.filter(function(countrie){
+    return String(countrie.continents).toLowerCase().includes(option.toLowerCase())
+  }))
+  toggleDropdown(); // Close the dropdown after selecting an option
 }
